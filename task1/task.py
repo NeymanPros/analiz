5820213adead3c5fe5ac4a0ffef7ab1e32276f9f
Alexey <113046879+NeymanPros@users.noreply.github.com>
2025-09-15 00:36:07 +0300

    actually create task1

--- task1/task.py
+++ task1/task.py
@@ -1,1 +1,15 @@
+import pandas as pd
 
+def reader(path):
+    data = pd.read_csv(path, header=None)
+
+    names = sorted(set(data[0]).union(set(data[1])))
+    result = pd.DataFrame(0, index=names, columns=names)
+
+    for x, y in data.itertuples(index=False):
+        result.loc[x, y] = 1
+
+    return result
+
+matrix = reader("analiz1.csv")
+print(matrix)
